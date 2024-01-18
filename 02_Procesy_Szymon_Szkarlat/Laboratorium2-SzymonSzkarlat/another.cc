80997ee893a04b628899bfc48f70721af4bd7d78
Szymon Szkarłat <115925925+sszkarlat@users.noreply.github.com>
2024-01-18 20:12:07 +0100

    Delete 02_Procesy_Szymon_Szkarlat/Laboratorium2-SzymonSzkarlat directory

--- 02_Procesy_Szymon_Szkarlat/Laboratorium2-SzymonSzkarlat/another.cc
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  	printf("Hello, I'm another program\n");
-	getchar();
-	return 0;
-}
--- 02_Procesy_Szymon_Szkarlat/Laboratorium2-SzymonSzkarlat/catchsignal.cc
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <csignal>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>		
